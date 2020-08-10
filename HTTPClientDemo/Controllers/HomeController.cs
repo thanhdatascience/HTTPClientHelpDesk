f5a750e67aa9126584c3e34753344c0ee0dd7542
Thanh Pham Trung <thanhpt@mapmaker.asia>
2020-08-10 22:28:26 +0700

    Add project files.

---
+++ HTTPClientDemo/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HTTPClientDemo.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+//using Newtonsoft.Json;
+
+namespace HTTPClientDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private static HttpClient _httpClient = new HttpClient();
+        public IEnumerable<Ticket> Tickets { get; set; }
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task<ActionResult<IEnumerable<Ticket>>> Index()
+        {
+            var url = "https://graph.facebook.com/";
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                url);
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                Tickets = await JsonSerializer.DeserializeAsync
+                    <IEnumerable<Ticket>>(responseStream);
+            }
+            else
+            {
+                Tickets = new List<Ticket>();
+            }
+
+            return Tickets.ToList();
+        }
+
+        public async Task<bool> HTTPPost(string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+       
+        
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    
+        
+    }
+}
---
+++ HTTPClientDemo/Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HTTPClientDemo.Models
+{
+    public class Ticket
+    {
+        //[Key]
+        //public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactAddress { get; set; }
+        public string ContactCompanyName { get; set; }
+        public Tag Tags { get; set; }
+        public Attribute Attributes { get; set; }
+
+
+
+    }
+}
